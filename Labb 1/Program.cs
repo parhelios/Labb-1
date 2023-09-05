e9186eadd0a2aa687ef3e79ab85203b8a3552104
Tobias Lindberg <143404279+parhelios@users.noreply.github.com>
2023-09-05 09:50:44 +0200

    no gods, no kings, only jävligt dåliga idéer

--- Labb 1/Program.cs
+++ Labb 1/Program.cs
@@ -5,9 +5,16 @@ Console.Write("Ange textsträng: ");
 string userInput = Console.ReadLine();
 string input = userInput.ToLower().Replace(" ", string.Empty).Trim();
 
-string onlyDigts = new String(userInput.Where(Char.IsDigit).ToArray());
+char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Å', 'Ä', 'Ö', 'a',
+    'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+    'x', 'y', 'z', 'å', 'ä', 'ö' };
 
-Console.WriteLine(onlyDigts);
+//string onlyDigts = new String(userInput.Where(Char.IsDigit).ToArray());
+
+//Console.WriteLine(onlyDigts);
 
 Console.WriteLine("----------------------------");
 
@@ -15,29 +22,38 @@ Console.WriteLine("----------------------------");
 char[] inputChar = input.ToCharArray();
 char[] outputChar = new char[inputChar.Length];
 
-List<int> outputList = CharacterPosition(input);
+//List<int> outputList = CharacterPosition(input);
 
-foreach (int index in outputList)
+string[] subStrings = input.Split(alphabet);
+
+foreach (string subString in subStrings)
 {
-    Console.WriteLine(index);
+    Console.WriteLine(subString);
 }
 
-Console.WriteLine("----------------------------");
+//foreach (int index in outputList)
+//{
+//    Console.WriteLine(index);
+//}
 
-foreach (char character in inputChar)
-{
-    Console.WriteLine(character);
+//Console.WriteLine("----------------------------");
 
-}
+//foreach (char character in inputChar)
+//{
+//    Console.WriteLine(character);
+
+//}
+
+//string[] subStringSection = new string[inputChar.Length];
+//for (int i = 0; i < subStringSection.Length; i++)
+//{
+//    subStringSection[i] = subStringSection[i].Substring(0, outputList[8]);
+//    Console.WriteLine(subStringSection[i]);
+//}
 
 
 
-//char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-//    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 
-//    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Å', 'Ä', 'Ö', 'a', 
-//    'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-//    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-//    'x', 'y', 'z', 'å', 'ä', 'ö' };
+
 
 //List<char> letterPos = new List<char>();
 //List<char> numList = new List<char>();
