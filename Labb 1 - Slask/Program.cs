2b29833fceb3afdedca2c69676083df8c1a7540f
Tobias Lindberg <143404279+parhelios@users.noreply.github.com>
2023-09-05 10:34:13 +0200

    ja det går fan inte framåt

--- Labb 1 - Slask/Program.cs
+++ Labb 1 - Slask/Program.cs
@@ -1,12 +1,122 @@
-﻿string a = "str123";
-string b = string.Empty;
-int val;
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
 
-for (int i = 0; i < a.Length; i++)
+Console.Write("Ange textsträng: ");
+string userInput = Console.ReadLine();
+string input = userInput.ToLower().Replace(" ", string.Empty).Trim();
+
+//string onlyDigts = new String(userInput.Where(Char.IsDigit).ToArray());
+
+//Console.WriteLine(onlyDigts);
+
+Console.WriteLine("----------------------------");
+
+
+char[] inputChar = input.ToCharArray();
+char[] outputChar = new char[inputChar.Length];
+
+//List<int> outputList = CharacterPosition(input);
+
+string[] subStrings = input.Split(alphabet);
+
+foreach (string subString in subStrings)
 {
-    if (Char.IsDigit(a[i]))
-        b += a[i];
+    Console.WriteLine(subString);
 }
 
-if (b.Length > 0)
-    val = int.Parse(b);
+//foreach (int index in outputList)
+//{
+//    Console.WriteLine(index);
+//}
+
+//Console.WriteLine("----------------------------");
+
+//foreach (char character in inputChar)
+//{
+//    Console.WriteLine(character);
+
+//}
+
+//string[] subStringSection = new string[inputChar.Length];
+//for (int i = 0; i < subStringSection.Length; i++)
+//{
+//    subStringSection[i] = subStringSection[i].Substring(0, outputList[8]);
+//    Console.WriteLine(subStringSection[i]);
+//}
+
+
+
+
+
+//List<char> letterPos = new List<char>();
+//List<char> numList = new List<char>();
+
+//for (int i = 0; i < inputChar.Length; i++)
+//{
+//    if (inputChar == alphabet)
+//    {
+//        letterPos.Add(alphabet[i]);
+//    }
+//}
+
+//foreach (char letter in letterPos)
+//{
+//    Console.WriteLine(letter);
+//}
+
+//for (int i = 0; i < inputChar.Length; i++)
+//{
+//    bool letterCheck = Char.IsDigit(inputChar[i]);
+//    if (letterCheck)
+//    {
+//        Console.WriteLine(letterCheck);
+
+//    }
+//    else
+//    {
+//        Console.WriteLine(letterCheck);
+//    }
+//}
+
+static List<int> CharacterPosition(string input)
+{
+    List<int> charIndex = new List<int>();
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (char.IsLetter(input[i]))
+        {
+            charIndex.Add(i);
+        }
+    }
+    return charIndex;
+}
+
+char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Å', 'Ä', 'Ö', 'a',
+    'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+    'x', 'y', 'z', 'å', 'ä', 'ö' };
+
+string[] subStrings = input.Split(alphabet);
+double[] numSubStrings = new double[subStrings.Length];
+
+for (int i = 0; i < subStrings.Length; i++)
+{
+    numSubStrings[i] = double.Parse(subStrings[i]);
+}
+
+
+
+//for (int i = 0; i < subStrings.Length; i++)
+//{
+//        if (subStrings[i] == "1")
+//    {
+//        Console.WriteLine(subStrings[i] + "1");
+//    }
+//    else if (subStrings[i] == "2")
+//    { 
+//        Console.WriteLine(subStrings[i] + "2");
+//    }
+//}
